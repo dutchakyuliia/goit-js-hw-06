7ce45477d0cdb6e63316d760ff64a93578875e91
Dutchak Yuliia <dutchakyuliia@gmail.com>
2023-03-26 12:13:48 +0300

    resolve 4 task

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,17 @@
-const counterValue = 0;
+const buttonDecrement = document.querySelector('[data-action="decrement"]');
+const valueEl = document.querySelector("#value");
+const buttonIncrement = document.querySelector('[data-action="increment"]');
+
+let counterValue = 0;
+
+const handlBtnDecrClick = () => {
+  counterValue = counterValue - 1;
+  valueEl.innerHTML = counterValue;
+};
+const handlBtnIncrrClick = () => {
+  counterValue = counterValue + 1;
+  valueEl.innerHTML = counterValue;
+};
+
+buttonDecrement.addEventListener("click", handlBtnDecrClick);
+buttonIncrement.addEventListener("click", handlBtnIncrrClick);
