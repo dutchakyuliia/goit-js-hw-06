782d2ea9f9b1d9a648f6f8693eb1babfb4d891d7
Dutchak Yuliia <dutchakyuliia@gmail.com>
2023-03-29 15:49:26 +0300

    resolve 10 task

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -8,11 +8,35 @@ function getRandomHexColor() {
 const btnCreate = document.querySelector("[data-create]");
 const btnDestroy = document.querySelector("[data-destroy]");
 const boxes = document.querySelector("#boxes");
+const inputEl = document.querySelector("#controls > input");
+
 
 function createBoxes(amount) {
-  for (i = 1; i < amount ; i++) {
+  console.log(amount)
+  for (let i = 0; i < amount; i++) {
     const divCreate = document.createElement("div");
+
+    const firstDiv = 3;
+    divCreate.style.width = (3 + i) * 10 + "px" ;
+    divCreate.style.height = (3 + i) * 10 + "px";
+    divCreate.style.backgroundColor = getRandomHexColor();
+
     boxes.append(divCreate);
   }
   
 };
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+};
+
+btnCreate.addEventListener('click', () => {
+  createBoxes(inputEl.value);
+});
+
+btnDestroy.addEventListener('click', () =>
+{
+  destroyBoxes();
+}
+)
+
